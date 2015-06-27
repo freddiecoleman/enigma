87c8727aaa2fdf2c2589616ec9aa6652ad638828
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-27 22:21:13 +0100

    initial implementation of machine only consisting of a plugboard

--- src/main/java/com/freddiecoleman/enigma/Enigma.java
+++ src/main/java/com/freddiecoleman/enigma/Enigma.java
@@ -7,9 +7,18 @@ package com.freddiecoleman.enigma;
 public final class Enigma {
 	
 	private final Plugboard plugboard;
+	StringBuilder encryptedMessage;
 	
 	public Enigma(Wire[] wires){
 		this.plugboard = new Plugboard(wires);
+	}
+	
+	public void input(char inputKey){
+		encryptedMessage.append(plugboard.convert(inputKey));
+	}
+	
+	public String encryptedMessage(){
+		return encryptedMessage.toString();
 	}
 
 }

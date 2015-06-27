5b5d3e5f42e1c7bcb361700f1f1d37ec86fe793d
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-27 21:42:06 +0100

    tests for keys without wires and multiple wires in one plugboard

--- src/test/java/com/freddiecoleman/enigma/PlugboardTest.java
+++ src/test/java/com/freddiecoleman/enigma/PlugboardTest.java
@@ -19,5 +19,27 @@ public class PlugboardTest {
 		Plugboard plugboard = new Plugboard(wires);
 		assertEquals('a', plugboard.convert('x'));
 	}
+	
+	@Test
+	public void testKeyWithoutWire() {
+		Wire[] wires = { new Wire('a', 'x') };
+		Plugboard plugboard = new Plugboard(wires);
+		assertEquals('p', plugboard.convert('p'));
+	}
+	
+	@Test
+	public void testMultipleKeyMappings() {
+		Wire[] wires = { new Wire('a', 'x'), new Wire('j', 'q'), new Wire('l', 'e'), new Wire('r', 's') };
+		Plugboard plugboard = new Plugboard(wires);
+		assertEquals('a', plugboard.convert('x'));
+		assertEquals('x', plugboard.convert('a'));
+		assertEquals('j', plugboard.convert('q'));
+		assertEquals('q', plugboard.convert('j'));
+		assertEquals('l', plugboard.convert('e'));
+		assertEquals('e', plugboard.convert('l'));
+		assertEquals('r', plugboard.convert('s'));
+		assertEquals('s', plugboard.convert('r'));
+		assertEquals('b', plugboard.convert('b'));
+	}
 
 }

0692c9544a6702f31cc0f1faa38a746d927182b6
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-27 22:30:08 +0100

    running partial enigma

--- src/main/java/com/freddiecoleman/enigma/Enigma.java
+++ src/main/java/com/freddiecoleman/enigma/Enigma.java
@@ -7,10 +7,25 @@ package com.freddiecoleman.enigma;
 public final class Enigma {
 	
 	private final Plugboard plugboard;
-	StringBuilder encryptedMessage;
+	private StringBuilder encryptedMessage;
 	
 	public Enigma(Wire[] wires){
 		this.plugboard = new Plugboard(wires);
+		this.encryptedMessage = new StringBuilder();
+	}
+	
+	public static void main(String[] args){
+		Wire[] wires = { new Wire('a', 'x') };
+		Enigma enigma = new Enigma(wires);
+		
+		enigma.input('a');
+		enigma.input('b');
+		enigma.input('c');
+		enigma.input('x');
+		enigma.input('y');
+		enigma.input('z');
+		
+		System.out.println(enigma.encryptedMessage());
 	}
 	
 	/**

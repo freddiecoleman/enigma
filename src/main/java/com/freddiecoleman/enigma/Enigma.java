78cd13a00857ec05f7d04c5c6ba97e6f7ca74cf5
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-27 22:22:05 +0100

    javadoc

--- src/main/java/com/freddiecoleman/enigma/Enigma.java
+++ src/main/java/com/freddiecoleman/enigma/Enigma.java
@@ -13,10 +13,18 @@ public final class Enigma {
 		this.plugboard = new Plugboard(wires);
 	}
 	
+	/**
+	 * Pressing a key on the keyboard
+	 * @param inputKey
+	 */
 	public void input(char inputKey){
 		encryptedMessage.append(plugboard.convert(inputKey));
 	}
 	
+	/**
+	 * Retrieves the encrypted message
+	 * @return
+	 */
 	public String encryptedMessage(){
 		return encryptedMessage.toString();
 	}

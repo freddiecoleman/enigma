6f9f8acb6e5f3fba0c16a8c9ed6c55bbee635fb1
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-27 22:57:49 +0100

    ability to assign keymapping to a rotor by passing in a substitution cipher

--- src/main/java/com/freddiecoleman/enigma/Rotor.java
+++ src/main/java/com/freddiecoleman/enigma/Rotor.java
@@ -1,9 +1,23 @@
 package com.freddiecoleman.enigma;
 
+import java.util.HashMap;
+
 /**
+ * Coded in a way that makes it easy to match known configurations by inputting substitution cipher as a string.
+ * Configurations found at https://en.wikipedia.org/wiki/Enigma_rotor_details#Rotor_wiring_tables
  * @author freddiecoleman
  *
  */
 public class Rotor {
+	
+	private final HashMap<Character, Character> keyMappings = new HashMap<Character, Character>();
+	
+	public Rotor(String substitutionCipher){
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		for(int i = 0; i < substitutionCipher.length(); i++){
+			keyMappings.put(alphabet.charAt(i), substitutionCipher.charAt(i));
+			keyMappings.put(substitutionCipher.charAt(i), alphabet.charAt(i));
+		}
+	}
 
 }
